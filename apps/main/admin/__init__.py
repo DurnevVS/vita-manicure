6053ec6e62d4d395dec6a0d7b68b061d31e5bde7
Lewbertsklol <lewbertsklol@gmail.com>
2024-07-12 08:49:33 +0700

    added pwa

--- apps/main/admin/__init__.py
+++ apps/main/admin/__init__.py
@@ -2,3 +2,7 @@ from .client import ClientAdmin
 from .window import WindowAdmin
 from .review import ReviewAdmin
 from .works import WorksAdmin
+
+from django.contrib import admin
+
+admin.site.index_template = 'admin/base_site.html'
--- config/settings.py
+++ config/settings.py
@@ -33,13 +33,8 @@ CSRF_TRUSTED_ORIGINS = [f'https://{host}' for host in ALLOWED_HOSTS]
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    "whitenoise.runserver_nostatic",
-    'django.contrib.staticfiles',
+    # pwa
+    'pwa',
 
     # Admin:
     'rangefilter',
@@ -53,6 +48,15 @@ INSTALLED_APPS = [
     'apps.main',
     'apps.bot',
     'apps.proxy',
+
+    # default
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    "whitenoise.runserver_nostatic",
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -195,3 +199,46 @@ LOGGING = {
         },
     }
 }
+
+# PWA
+
+PWA_APP_NAME = 'Admin panel'
+PWA_APP_DESCRIPTION = "My app description"
+PWA_APP_THEME_COLOR = '#0A0302'
+PWA_APP_BACKGROUND_COLOR = '#ffffff'
+PWA_APP_DISPLAY = 'standalone'
+PWA_APP_SCOPE = '/admin/'
+PWA_APP_ORIENTATION = 'any'
+PWA_APP_START_URL = '/admin/'
+PWA_APP_STATUS_BAR_COLOR = 'default'
+PWA_APP_ICONS = [
+    {
+        'src': '/static/images/icons/admin.svg',
+        'sizes': '160x160'
+    }
+]
+PWA_APP_ICONS_APPLE = [
+    {
+        'src': '/static/images/icons/admin.svg',
+        'sizes': '160x160'
+    }
+]
+PWA_APP_SPLASH_SCREEN = [
+    {'src': '/static/images/icons/splash-640x1136.png',
+     'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'}]
+PWA_APP_DIR = 'ltr'
+PWA_APP_LANG = 'en-US'
+PWA_APP_SHORTCUTS = [
+    {
+        'name': 'Shortcut',
+        'url': '/target',
+        'description': 'Shortcut to a page in my application'
+    }
+]
+PWA_APP_SCREENSHOTS = [
+    {
+        'src': '/static/images/icons/splash-750x1334.png',
+        'sizes': '750x1334',
+        "type": "image/png"
+    }
+]
